5fa7f52fea6b824a0e39ccfa199c293e4ab295a1
mitchejj <me>
2020-03-29 20:57:34 -0400

    CSS processing
    - using cssnano to minify and remove comments
    - using postcss-purgecss to remove unneeded styles
    - not using autoprefixer or cssnano nor purge in dev mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,33 @@
-module.exports = {
-  plugins: [
+const cssnano_options = {
+    preset: ['default', {
+        discardComments: {
+            removeAll: true,
+        },
+    }]
+}
+
+const purgecss_options = {
+    content: ['./build/**/*.html'],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+}
+
+const plugins = [
     require('tailwindcss'),
     require('postcss-nested'),
-  ],
+  ]
+
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push (
+        require('autoprefixer'),
+        require('cssnano')(cssnano_options),
+        require('@fullhuman/postcss-purgecss')(purgecss_options),
+        
+    )
 }
+
+
+module.exports = {
+  plugins
+};
+
